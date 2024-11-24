0dbe6745d25002d9ff4e78e8a46be533a6ef0c27
Nelman25 <jonelvillaver735@gmail.com>
2024-11-24 12:28:18 +0800

    done header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import nuLogo from "../assets/NU_shield.png";
+import profile from "../assets/FormalPic200x200.jpg";
 
 const Header = () => {
   return (
-    <header className="max-w-[1440px] mx-auto bg-[#2E2EAA] px-12 py-2 font-hostGrotest">
+    <header className="flex justify-between mx-auto bg-[#2E2EA1] px-8 py-2 font-montserrat border-b-4 border-b-yellow-500">
       <div className="flex items-center">
         <img
           src={nuLogo}
           alt="national university logo"
-          className="size-16 mr-2"
+          className="h-10 w-10 mr-2"
         />
-        <h1 className="text-3xl leading-6">ClassTrack</h1>
+        <h1 className="text-[1.125rem] text-white tracking-widest">CLASSTRACK</h1>
+      </div>
+      <div className="flex items-center max-w-[]">
+        <img src={profile} alt="user profile picture" className="rounded-full size-8 mr-2" />
+        <p className="text-white text-sm">Jonel Villaver</p>
       </div>
     </header>
   );

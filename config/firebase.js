9e72d3c97cb40544438c7d986cee5da7edb164dd
Nelman25 <jonelvillaver735@gmail.com>
2024-11-25 11:39:45 +0800

    added firebase and setup configs

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUQ5WiYh7CTiDcR-YzyyjQLfQht6Q29pk",
+  authDomain: "classtrack-4fda6.firebaseapp.com",
+  projectId: "classtrack-4fda6",
+  storageBucket: "classtrack-4fda6.firebasestorage.app",
+  messagingSenderId: "895870190134",
+  appId: "1:895870190134:web:ac3721a62349b9bfe58e97",
+  measurementId: "G-ML8ETDKBLZ",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

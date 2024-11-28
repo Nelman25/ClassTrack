f5462f072dac9b3cca33ab34307b120f665774c4
Nelman25 <jonelvillaver735@gmail.com>
2024-11-28 22:37:14 +0800

    changed firebase project

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,33 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCJD9sLFzbYHSbQ_At08zMYyJXvi2w_lgI",
+//   authDomain: "classtrack-remake.firebaseapp.com",
+//   projectId: "classtrack-remake",
+//   storageBucket: "classtrack-remake.firebasestorage.app",
+//   messagingSenderId: "565327386550",
+//   appId: "1:565327386550:web:30ee4caa9891ed06fd5317",
+//   measurementId: "G-4BPPSV8WN0",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUQ5WiYh7CTiDcR-YzyyjQLfQht6Q29pk",
-  authDomain: "classtrack-4fda6.firebaseapp.com",
-  projectId: "classtrack-4fda6",
-  storageBucket: "classtrack-4fda6.firebasestorage.app",
-  messagingSenderId: "895870190134",
-  appId: "1:895870190134:web:ac3721a62349b9bfe58e97",
-  measurementId: "G-ML8ETDKBLZ",
+  apiKey: "AIzaSyCJD9sLFzbYHSbQ_At08zMYyJXvi2w_lgI",
+  authDomain: "classtrack-remake.firebaseapp.com",
+  projectId: "classtrack-remake",
+  storageBucket: "classtrack-remake.firebasestorage.app",
+  messagingSenderId: "565327386550",
+  appId: "1:565327386550:web:30ee4caa9891ed06fd5317",
+  measurementId: "G-4BPPSV8WN0",
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);

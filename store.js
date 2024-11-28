f8b6e1abc95cf9eafd7c3d2f2164883b0ba129a3
Nelman25 <jonelvillaver735@gmail.com>
2024-11-28 22:37:45 +0800

    added section slice

--- store.js
+++ store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import gradeSlice from "./reducers/gradeSlice";
 import studentInfoSlice from "./reducers/studentInfoSlice";
+import sectionSlice from "./reducers/sectionSlice";
 
 const store = configureStore({
   reducer: {
     grade: gradeSlice,
     info: studentInfoSlice,
+    sections: sectionSlice,
   },
 });
 

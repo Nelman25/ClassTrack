eafddb5e6e256648946ea17655dd85abb18daa25
Nelman25 <jonelvillaver735@gmail.com>
2024-12-02 16:57:46 +0800

    add student modal

---
+++ src/components/AddStudentDialog.jsx
@@ -0,0 +1,95 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useSelector } from "react-redux";
+import { addStudent } from "../../services";
+
+export function AddStudentDialog() {
+  const selectedClassId = useSelector(
+    (state) => state.userActivity.selectedClassId
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const data = Object.fromEntries(formData.entries());
+    addStudent(data, selectedClassId);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="w-full bg-green-400 text-xl py-8 hover:bg-green-500">
+          Add Student
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add a student</DialogTitle>
+          <DialogDescription>
+            Enter necessary details of your student.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Fullname
+              </Label>
+              <Input id="name" name="name" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Email
+              </Label>
+              <Input
+                type="email"
+                name="email"
+                id="username"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Course
+              </Label>
+              <Input id="username" name="course" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Address
+              </Label>
+              <Input id="username" name="address" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Student number
+              </Label>
+              <Input
+                id="username"
+                name="studentNumber"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Add Student</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default AddStudentDialog;

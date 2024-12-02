d489ab53b00bb33d85be542b9ef992db43a95f87
Nelman25 <jonelvillaver735@gmail.com>
2024-12-02 16:59:13 +0800

    helper functions for db operations

---
+++ services.js
@@ -0,0 +1,23 @@
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "./config/firebase";
+
+export const createClass = async (newClass) => {
+  try {
+    const docRef = await addDoc(collection(db, "Classes"), newClass);
+    console.log("Class added with ID: ", docRef.id);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const addStudent = async (newStudent, classId) => {
+  try {
+    const docRef = await addDoc(
+      collection(db, `Classes/${classId}/Students`),
+      newStudent
+    );
+    console.log("Student added with ID : ", docRef.id);
+  } catch (error) {
+    console.error(error);
+  }
+};

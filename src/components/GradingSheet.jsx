c138839e5e7393fb3a4643260ba6eddf6062f7cf
Nelman25 <jonelvillaver735@gmail.com>
2024-12-30 15:17:46 +0800

    big changes on gradingsheet. Changed the layout to table elements for better organization. Updating values now working.

--- src/components/GradingSheet.jsx
+++ src/components/GradingSheet.jsx
@@ -1,72 +1,60 @@
+/* eslint-disable react/prop-types */
 import { useSelector } from "react-redux";
-import { gradeCategories } from "@/lib/constants";
-import GradeCell from "./GradeCell";
-import Loading from "./Loading";
+import StudentDataRow from "./StudentDataRow";
 
 const GradingSheet = () => {
   const grades = useSelector((state) => state.students.students);
-  const loading = useSelector((state) => state.students.loading);
 
   return (
-    <div className="h-[54rem] overflow-x-auto thin-scrollbar font-montserrat">
-      <header className="bg-[#2b2b8f] sticky top-0 w-full">
-        <ul className="grid grid-cols-[240px_480px_240px_240px_240px_240px] max-xl:grid-cols-[180px_360px_180px_180px_180px_180px] w-full items-center text-center text-white text-lg font-montserrat font-semibold">
-          {gradeCategories.map((category) => (
-            <li key={category} className="py-2 border-l">
-              {category}
-            </li>
-          ))}
-        </ul>
-      </header>
-      {loading ? (
-        <Loading />
-      ) : (
-        grades.map((student) => {
+    <table
+      className="w-full bg-slate-100 max-h-[10rem] overflow-x-auto thin-scrollbar"
+      border={1}
+    >
+      <thead className="bg-blue-600">
+        <tr className="bg-[#2b2b8f] text-white">
+          <th className="border border-white py-2" colSpan={1}>
+            Student name
+          </th>
+          <th className="border border-white" colSpan={5}>
+            Quizzes
+          </th>
+          <th className="border border-white" colSpan={3}>
+            Lab Activities
+          </th>
+          <th className="border border-white" colSpan={1}>
+            Project
+          </th>
+          <th className="border border-white" colSpan={1}>
+            Final Exam
+          </th>
+          <th className="border border-white" colSpan={1}>
+            Final Grade
+          </th>
+        </tr>
+        <tr className="bg-blue-400 text-white">
+          <th className="gradeCell py-1">Full name</th>
+          <th className="gradeCell">Quiz 1</th>
+          <th className="gradeCell">Quiz 2</th>
+          <th className="gradeCell">Quiz 3</th>
+          <th className="gradeCell">Quiz 4</th>
+          <th className="gradeCell">Quiz 5</th>
+          <th className="gradeCell">Lab 1</th>
+          <th className="gradeCell">Lab 2</th>
+          <th className="gradeCell">Lab 3</th>
+          <th className="gradeCell">Project Score</th>
+          <th className="gradeCell">Exam Score</th>
+          <th className="gradeCell">Grade</th>
+        </tr>
+      </thead>
+
+      <tbody className="">
+        {grades.map((student) => {
           return (
-            <div
-              key={student.studentNumber}
-              className="grid grid-cols-[240px_480px_240px_240px_240px_240px] max-xl:grid-cols-[180px_360px_180px_180px_180px_180px] even:bg-blue-100 odd:bg-white"
-            >
-              <p className="px-2 py-4 text-sm border-b border-b-slate-300 max-w-[240px] max-xl:max-w-[180px]">
-                {student.name}
-              </p>
-              <div className="grid grid-cols-5 text-center min-w-[480px] max-xl:min-w-[360px]">
-                {(() => {
-                  const inputs = [];
-                  for (let i = 1; i <= 5; i++) {
-                    inputs.push(
-                      <GradeCell
-                        key={`quiz${i}`}
-                        value={student.quizzes[`quiz${i}`]}
-                      />
-                    );
-                  }
-                  return inputs;
-                })()}
-              </div>
-              <div className="grid grid-cols-3 text-center min-w-[240px] max-xl:min-w-[180px]">
-                {(() => {
-                  const inputs = [];
-                  for (let i = 1; i <= 3; i++) {
-                    inputs.push(
-                      <GradeCell
-                        key={`lab${i}`}
-                        value={student.labScores[`lab${i}`]}
-                        className=""
-                      />
-                    );
-                  }
-                  return inputs;
-                })()}
-              </div>
-              <GradeCell value={student.project} className="" />
-              <GradeCell value={student.finalExam} className="" />
-              <GradeCell value={student.finalExam} className="" />
-            </div>
+            <StudentDataRow key={student.studentNumber} student={student} />
           );
-        })
-      )}
-    </div>
+        })}
+      </tbody>
+    </table>
   );
 };
 

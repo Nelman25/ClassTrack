167883f234d14b6c4fa5b01c42c408d6d8ac0b0e
Nelman25 <jonelvillaver735@gmail.com>
2025-01-04 09:39:55 +0800

    added gradesChanges state

--- reducers/userActivitySlice.js
+++ reducers/userActivitySlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  selectedClassId: "",
+  classData: {},
+  gradesChanges: [],
 };
 
 const userActivitySlice = createSlice({
@@ -9,12 +10,28 @@ const userActivitySlice = createSlice({
   initialState,
   reducers: {
     setSelectedClass: (state, action) => {
-      state.selectedClassId = action.payload;
+      state.classData = action.payload;
+    },
+    setGradesChanges: (state, action) => {
+      state.gradesChanges = state.gradesChanges.map((change) => {
+        if (change.docId === action.payload.docId) {
+          return { ...change, ...action.payload };
+        }
+        return change;
+      });
+
+      const isExisting = state.gradesChanges.some(
+        (change) => change.docId === action.payload.docId
+      );
+
+      if (!isExisting) {
+        state.gradesChanges.push(action.payload);
+      }
     },
   },
 });
 
 export const currentSelectedClassId = (state) =>
   state.userActivity.selectedClassId;
-export const { setSelectedClass } = userActivitySlice.actions;
+export const { setSelectedClass, setGradesChanges } = userActivitySlice.actions;
 export default userActivitySlice.reducer;
